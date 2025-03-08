2d11e9b750413f77becefba76562da84352ecac0
gauri0728 <100562602+gauri0728@users.noreply.github.com>
2025-03-08 14:21:29 +0530

    4th commit

--- womenHackethon/src/App.jsx
+++ womenHackethon/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import "./global.css";
+import Products from "./pages/Products";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
@@ -12,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/products" element={<Products />} />
       </Routes>
     </Router>
   );
